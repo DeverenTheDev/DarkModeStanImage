f1ddcad88fca7fb75d0f023074fc3c4f58109c0e
Deveren Schultz <56973786+DeverenTheDev@users.noreply.github.com>
2026-07-10 17:55:13 -0600

    Update injected.js
    stop the disorientating redraws the small new strip that scrolled into view.

--- injected.js
+++ injected.js
@@ -3,16 +3,23 @@
   const proto = CanvasRenderingContext2D.prototype;
   const originalDrawImage = proto.drawImage;
 
-  proto.drawImage = function (...args) {
-    if (document.documentElement.hasAttribute(ATTR)) {
+  function isBlitSource(source) {
+    return (
+      (typeof HTMLCanvasElement !== 'undefined' && source instanceof HTMLCanvasElement) ||
+      (typeof OffscreenCanvas !== 'undefined' && source instanceof OffscreenCanvas)
+    );
+  }
+
+  proto.drawImage = function (source, ...rest) {
+    if (document.documentElement.hasAttribute(ATTR) && !isBlitSource(source)) {
       const prevFilter = this.filter;
       this.filter =
         (prevFilter && prevFilter !== 'none' ? prevFilter + ' ' : '') +
         'invert(1) hue-rotate(180deg)';
-      const result = originalDrawImage.apply(this, args);
+      const result = originalDrawImage.call(this, source, ...rest);
       this.filter = prevFilter;
       return result;
     }
-    return originalDrawImage.apply(this, args);
+    return originalDrawImage.call(this, source, ...rest);
   };
 })();
